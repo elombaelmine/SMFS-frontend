db21e9c40a75efd50c92af697be9a7e842f62b60
Elomba Elmine <elmine0520@gmail.com>
2026-05-22 10:27:43 +0100

    adding dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { Settings } from './pages/settings/settings';
 import { Support } from './pages/support/support';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: 'auth', component: Auth },
   { path: 'settings', component: Settings },
   { path: 'support', component: Support },
+  { path: 'dashboard', component: Dashboard},
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
-  { path: '**', redirectTo: '/auth' }
+  { path: '**', redirectTo: '/auth' },
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
