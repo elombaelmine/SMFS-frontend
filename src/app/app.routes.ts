6cd6953c6d20dfbd65dab75785ad91e60d231209
moiseheranetchatchou@gmail.com <moiseheranetchatchou@gmail.com>
2026-05-22 05:22:55 -0700

    dashboard and report

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes } from '@angular/router';
 import { Auth } from './pages/auth/auth';
+import { Dashboard } from './pages/dashboard/dashboard';
 import { Settings } from './pages/settings/settings';
 import { Support } from './pages/support/support';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Reports } from './pages/reports/reports';
 
 export const routes: Routes = [
   { path: 'auth', component: Auth },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'reports', component: Reports },
   { path: 'settings', component: Settings },
   { path: 'support', component: Support },
-  { path: '', redirectTo: '/auth', pathMatch: 'full' },
+
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: '/auth' }
 ];
 @NgModule({
